2212077c851dcdb29dd3e309d159e99ed6075386
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-06 09:20:01 +0200

    init commit

---
+++ c02/ex06/ft_str_is_printable.c
@@ -0,0 +1,21 @@
+int ft_str_is_printable(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] < 32 || str[i] > 126)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+#include <stdio.h>
+int main()
+{
+    char *src = "vdfbd\n";
+    printf("%d\n", ft_str_is_printable(src));
+    return 0;
+}
