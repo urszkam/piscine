be1cdc2531e276afb049faa9f849f13c0da8e0e1
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-09 16:45:31 +0200

    .

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,14 @@
+#ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN(nbr) nbr % 2 == 0
+# include <unistd.h>
+
+typedef int	t_bool;
+
+#endif
