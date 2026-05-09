e5d1ac66ba8f4a4d2846370c11c68fb052fa638a
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-09 13:38:35 +0200

    .

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,27 @@
+int	ft_is_prime(int nb)
+{
+	int	div;
+
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	div = 3;
+	while (div < nb)
+	{
+		if (nb % div == 0)
+			return (0);
+		div += 2;
+	}
+	return (1);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int i, char **v)
+{
+	if (i > 1)
+		printf("%d\n", ft_is_prime(atoi(v[1])));
+}
