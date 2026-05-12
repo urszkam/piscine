b946826b19f554fd69333de7db3d03b407ba0701
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-12 22:14:41 +0200

    init commit

---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	size;
+	int	*arr;
+
+	if (min >= max)
+		return (NULL);
+	size = max - min;
+	if (size < 0)
+		size = -size;
+	arr = (int *)malloc(sizeof(int) * (size - 1));
+	if (!arr)
+		return (NULL);
+	while (size--)
+		arr[size] = --max;
+	return (arr);
+}
+
+#include <stdio.h>
+int main()
+{
+	int max = -3;
+	int min = -6;
+	int *arr = ft_range(min, max);
+	for (int i = 0; min < max; min++, i++)
+		printf("%d\n", arr[i]);
+}
