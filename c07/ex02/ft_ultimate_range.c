5c238106e034e9cb7ed747c2da5479c87ec6870d
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-15 18:29:11 +0200

    init commit

---
+++ c07/ex02/ft_ultimate_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	size;
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	size = max - min;
+	*range = (int *)malloc(sizeof(int) * size);
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (i < size)
+		(*range)[i++] = min++;
+	return (size);
+}
+
+#include <stdio.h>
+int main()
+{
+        int max = -3;
+        int min = -6;
+	int *arr;
+        ft_ultimate_range(&arr, min, max);
+        for (int i = 0; min < max; min++, i++)
+                printf("%d\n", arr[i]);
+}
+
