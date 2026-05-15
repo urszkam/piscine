eee355e7424579681b927348e49bdc29f02f4e7e
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-15 18:29:05 +0200

    init commit

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -8,9 +8,7 @@ int	*ft_range(int min, int max)
 	if (min >= max)
 		return (NULL);
 	size = max - min;
-	if (size < 0)
-		size = -size;
-	arr = (int *)malloc(sizeof(int) * (size - 1));
+	arr = (int *)malloc(sizeof(int) * size);
 	if (!arr)
 		return (NULL);
 	while (size--)
