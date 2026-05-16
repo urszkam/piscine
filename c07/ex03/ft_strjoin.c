99ccee752c88373efd9a5c554466fe1d5b8d6f94
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-16 08:17:32 +0200

    init commit

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int	ft_strlen(char *str)
+int	_ft_strlen(char *str)
 {
 	int	i;
 
@@ -10,16 +11,18 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int	tot_size(int size, char **strs, char *sep)
+int	_tot_size(int size, char **strs, char *sep)
 {
 	int	i;
+	int	total_size;
 
 	if (size == 0)
 		return (0);
 	i = 0;
+	total_size = 0;
 	while (i < size)
-		total_size += ft_strlen(strs[i++]);
-	total_size += ft_strlen(sep) * (size - 1);
+		total_size += _ft_strlen(strs[i++]);
+	total_size += _ft_strlen(sep) * (size - 1);
 	return (total_size);
 }
 
@@ -28,21 +31,30 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	char	*dst;
 	int		i;
 	int		j;
+	int		k;
 
-	dst = (char *)malloc(sizeof(char) * (tot_size(size, strs, sep) + 1));
+	dst = (char *)malloc(sizeof(char) * (_tot_size(size, strs, sep) + 1));
 	if (!dst)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (i < size)
 	{
-		while (*strs[i])
-		{
-			dst[j++] = *strs[i];
-			**strs++;
-		}
+		k = 0;
+		while (strs[i][k])
+			dst[j++] = strs[i][k++];
+		k = 0;
+		while (i < size - 1 && sep[k])
+			dst[j++] = sep[k++];
 		i++;
 	}
 	dst[j] = 0;
 	return (dst);
 }
+/*
+int main()
+{
+	char *arr[] = {"Hello", "World"};
+	char *s = ft_strjoin(2, arr, "...");
+	printf("%s", s);
+}*/
