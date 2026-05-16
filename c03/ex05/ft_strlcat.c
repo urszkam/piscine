e25a9e0d02aa6884c837cfcb3c81adf37b3c20fc
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-16 21:51:27 +0200

    ." . "

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -13,19 +13,19 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	len_d;
 	unsigned int	len_s;
 	unsigned int	i;
-    
-	len_d = ft_strlen(dest);
+
+	len_d = 0;
 	len_s = ft_strlen(src);
 	i = 0;
-	if (size > len_d)
+	while (len_d < size && dest[len_d])
+		len_d++;
+	if (len_d == size)
+		return (size + len_s);
+	while (src[i] && len_d + i < size - 1)
 	{
-		while (src[i] && i < size - len_d - 1)
-		{
-			dest[len_d + i] = src[i];
-			i++;
-		}
-		dest[len_d + i] = '\0';
-		return (len_d + len_s);
+		dest[len_d + i] = src[i];
+		i++;
 	}
-	return (size + len_s);
+	dest[len_d + i] = '\0';
+	return (len_d + len_s);
 }
