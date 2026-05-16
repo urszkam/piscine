413e4ff3e1b30bc029b180fbf7d81fa0c3bbfcbb
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-16 21:51:08 +0200

    .

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -15,21 +15,20 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (i < n)
 	{
-	if (!s1[i] || s1[i] != s2[i])
-		return ((unsigned char) s1[i] - (unsigned char) s2[i]);
-	i++;
+		if (!s1[i] || s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
 	}
 	return (0);
 }
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	unsigned int	i;
-	unsigned int	j;
 	unsigned int	len;
 
 	len = ft_strlen(to_find);
-	i = 0;
+	if (len == 0)
+		return (str);
 	while (*str)
 	{
 		if (ft_strncmp(str, to_find, len) == 0)
@@ -37,15 +36,4 @@ char	*ft_strstr(char *str, char *to_find)
 		str++;
 	}
 	return (0);
-}
-
-#include <string.h>
-#include <stdio.h>
-int main(void)
-{
-    char *str = "Hello World Blah Blah";
-    char *to_find = "World";
-    printf("%s|\n", ft_strstr(str, to_find));
-    printf("%s|", strstr(str, to_find));
-    return (0);
 }
