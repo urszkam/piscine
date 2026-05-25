9dd080a759a5cc418aceb80b0a47624078456017
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-05-25 16:20:19 +0200

    init commit

--- c11/ex01/ft_map.c
+++ c11/ex01/ft_map.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int	*ft_map(int *tab, int length, int(*f)(int))
+int	*ft_map(int *tab, int length, int (*f)(int))
 {
 	int	*tab_dest;
 	int	idx;
@@ -15,4 +16,25 @@ int	*ft_map(int *tab, int length, int(*f)(int))
 		idx++;
 	}
 	return (tab_dest);
+}
+
+int	add_one(int n)
+{
+	return (n + 1);
+}
+
+int	main(void)
+{
+	int	tab[] = {1, 2, 3, 4, 5};
+	int	*result;
+	int	i;
+
+	result = ft_map(tab, 5, &add_one);
+	i = 0;
+	while (i < 5)
+	{
+		printf("%d\n", result[i]);
+		i++;
+	}
+	return (0);
 }
